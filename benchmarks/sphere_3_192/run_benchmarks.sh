0dc8dc9c9dd444951928dcf6c59967f9f617f488
Ulrich Bauer <mail@ulrich-bauer.org>
2017-01-09 11:56:45 +0100

    benchmark H^1

---
+++ benchmarks/sphere_3_192/run_benchmarks.sh
@@ -0,0 +1,4 @@
+. run_dionysus.sh
+. run_dipha.sh
+. run_gudhi.sh
+. run_ripser.sh
---
+++ benchmarks/sphere_3_192/run_dionysus.sh
@@ -0,0 +1,1 @@
+/usr/bin/time -l ~/Source/Dionysus/examples/cohomology/rips-pairwise-cohomology -s2 -p2 ~/Bitbucket/phat-paper/benchmark/point\ cloud/sphere_3_192_points.dat 2>&1 | tee dionysus.out.txt
