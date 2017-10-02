8c8425e93d995624c9ced79c7c25de45ead4e48b
Rann Bar-On <rann@math.duke.edu>
2017-10-02 18:08:16 -0400

    hack to compile under python 2 and 3

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import numpy
 c_ext = Extension("ripser", 
 sources = ["ripser.cpp", "_ripser.cpp"],
 define_macros=[("USE_COEFFICIENTS", 1), ("ASSEMBLE_REDUCTION_MATRIX", 1), ("PYTHON_EXTENSION", 1)],
-extra_compile_args = ["-std=c++11"],
+extra_compile_args = ["-std=c++11", "-fpermissive"],
 )
 
 setup(name="ripser",
