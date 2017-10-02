aaff5e7f29bd9df66d22fb19eb253f97f47ed879
Rann Bar-On <rann@math.duke.edu>
2017-10-02 18:15:27 -0400

    Slightly better hack

--- setup.py
+++ setup.py
@@ -1,10 +1,16 @@
 from distutils.core import setup, Extension
 import numpy
+import sys
+
+options = ["-std=c++11"]
+
+if sys.version_info[0] == 2:
+	options.append("-fpermissive")
 
 c_ext = Extension("ripser", 
 sources = ["ripser.cpp", "_ripser.cpp"],
 define_macros=[("USE_COEFFICIENTS", 1), ("ASSEMBLE_REDUCTION_MATRIX", 1), ("PYTHON_EXTENSION", 1)],
-extra_compile_args = ["-std=c++11", "-fpermissive"],
+extra_compile_args = options,
 )
 
 setup(name="ripser",
