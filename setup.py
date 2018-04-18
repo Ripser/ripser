7a3572b0a2e69624392e284f65b41b37fa54eb33
Nathaniel Saul <nat@saulgill.com>
2018-04-17 21:08:58 -0700

    pep8 and fixes so pypi markdown renders

--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
 import sys
-from distutils.core import setup, Extension
+
+from setuptools import setup
+from setuptools.extension import Extension
 
 # we'd better have Cython installed, or it's a no-go
 try:
-	from Cython.Build import cythonize
-	from Cython.Distutils import build_ext
+    from Cython.Build import cythonize
+    from Cython.Distutils import build_ext
 except:
     print("You don't seem to have Cython installed. Please get a")
     print("copy from www.cython.org or install it with `pip install Cython`")
@@ -14,52 +16,51 @@ except:
 with open('README.md') as f:
     long_description = f.read()
 
-#c++ -std=c++11 ripser.cpp -o ripser -Ofast -D NDEBUG -D PRINT_PERSISTENCE_PAIRS
+# c++ -std=c++11 ripser.cpp -o ripser -Ofast -D NDEBUG -D PRINT_PERSISTENCE_PAIRS
 #options = ["-std=c++11", "-Ofast"]
 options = ["-std=c++11", "-Ofast"]
 
 if sys.version_info[0] == 2:
-	options.append("-fpermissive")
+    options.append("-fpermissive")
+
 
 class CustomBuildExtCommand(build_ext):
-	""" This extension command lets us not require numpy be installed before running pip install ripser """
-	"""build_ext command for use when numpy headers are needed."""
-	def run(self):
-		# Import numpy here, only when headers are needed
-		import numpy
-		# Add numpy headers to include_dirs
-		self.include_dirs.append(numpy.get_include())
-		# Call original build_ext command
-		build_ext.run(self)
+    """ This extension command lets us not require numpy be installed before running pip install ripser """
+    """build_ext command for use when numpy headers are needed."""
 
-setup(
-	name="ripser",
-	version='0.1.5',	
-	description="A Lean Persistent Homology Library for Python",
-	long_description=long_description,
-	long_description_content_type="text/markdown",
-	author="Chris Tralie, Nathaniel Saul",
-	author_email="chris.tralie@gmail.com, nathaniel.saul@wsu.edu",
-	url="https://github.com/ctralie/ripser",
-	license='LGPL',
-	packages=['ripser'],
-
-	ext_modules = cythonize(
-	Extension("pyRipser",
-		sources = ["ripser/pyRipser.pyx"],
-		define_macros=[("USE_COEFFICIENTS", 1), ("PYTHON_EXTENSION", 1), ("NDEBUG", 1), ("ASSEMBLE_REDUCTION_MATRIX", 1)],
-		extra_compile_args = options,
-		language="c++"
-	)),
-
-	install_requires=[
-		'Cython',
-		'numpy',
-		'scipy',
-        'matplotlib',
-		'scikit-learn'
-    ],
-	cmdclass = {'build_ext': CustomBuildExtCommand},
+    def run(self):
+        # Import numpy here, only when headers are needed
+        import numpy
+        # Add numpy headers to include_dirs
+        self.include_dirs.append(numpy.get_include())
+        # Call original build_ext command
+        build_ext.run(self)
 
 
-)
+setup(name="ripser",
+      version='0.1.6',
+      description="A Lean Persistent Homology Library for Python",
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      author="Chris Tralie, Nathaniel Saul",
+      author_email="chris.tralie@gmail.com, nathaniel.saul@wsu.edu",
+      url="https://github.com/ctralie/ripser",
+      license='LGPL',
+      packages=['ripser'],
+      ext_modules=cythonize(Extension("pyRipser",
+                                      sources=["ripser/pyRipser.pyx"],
+                                      define_macros=[("USE_COEFFICIENTS", 1), ("PYTHON_EXTENSION", 1),
+                                                     ("NDEBUG", 1), ("ASSEMBLE_REDUCTION_MATRIX", 1)],
+                                      extra_compile_args=options,
+                                      language="c++"
+                                      )),
+
+      install_requires=[
+          'Cython',
+          'numpy',
+          'scipy',
+          'matplotlib',
+          'scikit-learn'
+      ],
+      cmdclass={'build_ext': CustomBuildExtCommand},
+      )
