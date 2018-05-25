268f3b9c5b25082bd0f10c44622945a24f3185fa
Ulrich Bauer <mail@ulrich-bauer.org>
2018-05-24 20:51:47 -0400

    cleanup

--- ripser.cpp
+++ ripser.cpp
@@ -26,8 +26,6 @@ with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 //#define USE_GOOGLE_HASHMAP
 
-#include "prettyprint.hpp"
-
 #include <cassert>
 #include <cmath>
 #include <fstream>
